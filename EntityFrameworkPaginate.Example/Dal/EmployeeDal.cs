1f42186fc054d06db41a86b63f8b06a07527459b
harshwPro <harshw1@gmail.com>
2017-03-30 19:39:49 +0530

    Example modified and nuspec updated.

--- EntityFrameworkPaginate.Example/Dal/EmployeeDal.cs
+++ EntityFrameworkPaginate.Example/Dal/EmployeeDal.cs
@@ -6,14 +6,13 @@
         {
             Page<Employee> employees;
             var filters = new Filters<Employee>();
-            var sorts = new Sorts<Employee>();
+            filters.Add(!string.IsNullOrEmpty(searchText), x=>x.LoginID.Contains(searchText));
+            filters.Add(!string.IsNullOrEmpty(jobTitle), x=> x.JobTitle.Equals(jobTitle));
 
-            filters.Add(!string.IsNullOrEmpty(searchText), x => x.LoginID.Contains(searchText));
-            filters.Add(!string.IsNullOrEmpty(jobTitle), x => x.JobTitle.Equals(jobTitle));
-            
-            sorts.Add(sortBy == 1, x => x.BusinessEntityID);
-            sorts.Add(sortBy == 2, x => x.LoginID);
-            sorts.Add(sortBy == 3, x => x.JobTitle);
+            var sorts = new Sorts<Employee>();
+            sorts.Add(sortBy == 1, x=>x.BusinessEntityID);
+            sorts.Add(sortBy == 2, x=>x.LoginID);
+            sorts.Add(sortBy == 3, x=>x.JobTitle);
 
             using (var context = new AdventureWorksEntities())
             {
