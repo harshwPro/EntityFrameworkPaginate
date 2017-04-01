76b6a86fcc88709803a252e08f5a5a4480267891
harshwPro <harshw1@gmail.com>
2017-04-01 12:14:36 +0530

    InfiniteScroll project commit.

---
+++ InfiniteScroll/Controllers/JqueryExampleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InfiniteScroll.Controllers
+{
+    public class JqueryExampleController : Controller
+    {
+        // GET: JqueryExample
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
